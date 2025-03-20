c33af101835590d6264aa94698f841bb73b4bf3d
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-20 13:25:29 +0100

    Implemented fully n! method

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -24,6 +24,13 @@ public class Calculator {
     }
 
     public int factorial(int n) {
-        return 0;
+        if(n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        }
+        else {
+            return n * factorial(n-1);
+        }
     }
 }
